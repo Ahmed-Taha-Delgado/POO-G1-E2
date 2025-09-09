58c745a3eb89ff5bdbac2e93eb9b0c8559ce3ed7
Ahmed-Taha-Delgado <ahmedtmtd@gmail.com>
2025-09-09 15:49:04 -0600

    Reto bien

--- Practica3/Reto/FormulaGeneral.java
+++ Practica3/Reto/FormulaGeneral.java
@@ -2,33 +2,29 @@ public class FormulaGeneral{
 
     public static void main(String[] args) {
         
-        if(args.length==0){
+        if(args.length!=3){
             System.out.println("Ingresa los valores de a, b, c");
             return;
         }
 
-        int imaginario=0;
-
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
 
 
         double det=(b*b)-((4*a*c));
 
         if(det<0){
             det = Math.abs(det);
-            imaginario=1;
-        }
-
-        double x1 = ((-b)+Math.sqrt(det))/(2*a);
-        double x2 = ((-b)-Math.sqrt(det))/(2*a);
-
-        if(imaginario==1){
-            System.out.println("x = "+ x1 +"i" + ", " + x2 + "i");
+            double xi=Math.sqrt(det)/(2*a);
+            double xr=(-b)/(2*a);
+            System.out.println("x = " + xr + " + " + xi + "i" + ", " + xr + " - " + xi + "i");
         }else{
+            double x1 = ((-b)+Math.sqrt(det))/(2*a);
+            double x2 = ((-b)-Math.sqrt(det))/(2*a);
             System.out.println("x = "+ x1 + " , " + x2);
         }
+
     }
 
 }
